dafe40801ab0bf82a376cc1a2ef42124e6e592fa
BenficaS <107698702+BenficaS@users.noreply.github.com>
2023-04-15 13:02:01 -0300

    Delete TesteJavaxSwing-master directory

--- TesteJavaxSwing-master/src/TesteJframe.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-public class TesteJframe {
-    public static void main(String[] args) {
-        JFrame janela=new JFrame("Meu primeiro frame em java");
-        janela.setSize(400,400);
-        janela.setVisible(true);
-    }
-}
