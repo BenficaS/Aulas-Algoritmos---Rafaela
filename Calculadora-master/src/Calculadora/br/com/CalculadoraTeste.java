41f996e19319ab0b49baa1eaa118895798768a3d
BenficaS <107698702+BenficaS@users.noreply.github.com>
2023-04-15 13:01:30 -0300

    Delete Calculadora-master directory

--- Calculadora-master/src/Calculadora/br/com/CalculadoraTeste.java
+++
@@ -1,8 +0,0 @@
-package Calculadora.br.com;
-
-public class CalculadoraTeste extends Calculadora {
-    public static void main(String[] args) {
-        Calculadora calculadora = new Calculadora();
-        calculadora.calculadora();
-    }
-}
